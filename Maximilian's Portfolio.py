383f1415933e71b12ce0e101de81ca9472fd12f6
MaxCSiry <max.c.siry@gmail.com>
2025-09-25 14:06:08 -0400

    Add files via upload

---
+++ Maximilian's Portfolio.py
@@ -0,0 +1,77 @@
+
+#This File will contain the information to be displayed in your portfolio
+
+#CHANGE BELOW
+profile_picture = "Downloads/IMG_8218"
+about_me = "I'm Maximilian Siry. I study at the Georgia Institute of Technology and I major in Mathematics. "
+
+
+#CHANGE BELOW (OPTIONAL)
+linkedin_image_url = "https://content.linkedin.com/content/dam/me/business/en-us/amp/brand-site/v2/bg/LI-Bug.svg.original.svg"
+github_image_url = "https://cdn-icons-png.flaticon.com/256/25/25231.png"
+email_image_url = "https://logowik.com/content/uploads/images/513_email.jpg"
+
+#CHANGE BELOW
+my_linkedin_url = "https://www.linkedin.com/in/maximilian-siry-770472353/"
+my_github_url = "https://github.com/MaxCSiry/GaTechPortfolio.git"
+my_email_address = "max.c.siry@gmail.com"
+
+
+education_data ={
+    'Major': 'Bachelor of Science in Mathematics',
+    'Institution': 'Georgia Institute of Technology',
+    'Location': 'Atlanta, GA',
+    'Graduation Date': 'May, 2028',
+    'GPA': 'N/A'
+}
+course_data = {
+    "code":["CS 1301", "ECON 2251", "MATH 1554", "APPH 1040"], 
+    "names":["Intro to CS", "Statistics for Economics", "Linear Algebra", "Applied Physical Health"], 
+    "semester_taken":["1st", "1st", "1st", "1st"],
+    "skills":["Python is not my favorite snake anymore", "R Studio wants my soul", "Basically row reduction, but everything wants to steal your soul", "At least it's helping me be a better person"],
+    }
+experience_data = {
+    "Accounting Intern at URS Compliance": (["- Worked on job sheets, master CHRON files, and Quickbooks",
+                                                                          "- Helped to increase productivity", "- Made Mrs. Watson happy"],"),
+    "Volunteer for St. Lawrence Catholic Church":(["- Was an effective peer teen leader, and altar server",
+                                                           "- Helped to have clear and clean masses for 8 years"]),
+    "Jelly Fish Catcher":(["- Caught 100+ Jellyfish with the help of my colleague Patrick"],"Images/jelly.jpg")
+
+}
+
+projects_data = {
+    "GSMST German Honor Society Wixsite": "Created the current German Honor Society and tailored it from a basic model on Google sites",
+}
+
+programming_data = {
+    "Python": 90,
+    "Java": 70,
+    "C": 40,
+}
+
+#CHANGE BELOW (OPTIONAL)
+programming_icons = {
+    "Python": "🐍",
+    "R": "R",
+    "Matlab": "🔍",
+}
+spoken_icons = {"Polish": "PL",
+    "English": "🇬🇧",
+    "German":"DE"
+}
+
+#CHANGE BELOW
+spoken_data = {
+    "English": "Fluent",
+    "German": "Intermediate",
+    "Polish": "Intermediate",
+}
+leadership_data = {
+    "Leadership Certification at GSMST": (["- Used to become an executive officer of Debate Club"]),
+    "Peer Teen Leader at St. Lawrence Catholic Church": (["- Was an effective catechist for the Youth Alpha program"])
+
+}
+activity_data={
+    "Debate Club": ["- Helped to organize meetings and create website using google sites and wix", 
+            "- Was a part of weekly meetings"]
+}
