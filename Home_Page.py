faa746de2d35faac37b51fa1f070f02938b1e856
MaxCSiry <max.c.siry@gmail.com>
2025-09-25 14:28:10 -0400

    Update Home_Page.py

--- Home_Page.py
+++ Home_Page.py
@@ -7,22 +7,22 @@ st.title("Web Development Lab01")
 # TODO: Fill out your team number, section, and team members
 
 st.header("CS 1301")
-st.subheader("Web Development - Section X")
-st.subheader("Name")
+st.subheader("Web Development - Section E")
+st.subheader("Maximilian Siry")
 
 
 # Introduction
 # TODO: Write a quick description for all of your pages in this lab below, in the form:
-#       1. **Page Name**: Description
-#       2. **Page Name**: Description
+#       1. **Spongebob's Portfolio**: Spongebob's Example Portfolio
+#       2. **Maximilian's Portfolio**: Maximilian's own portfolio
 #       3. **Page Name**: Description
 #       4. **Page Name**: Description
 
 st.write("""
 Welcome to our Streamlit Web Development Lab01 app! You can navigate between the pages using the sidebar to the left. The following pages are:
 
-1.
-2.
+1. **Spongebob's Portfolio**: Spongebob's Own Portfolio
+2. **Maximilian's Portfolio**: My Own Portfolio
 3.
 4.
 
